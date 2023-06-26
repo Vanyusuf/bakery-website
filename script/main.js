190eb3ab89269f8c9a5bba2d72d873106b47af05
vanyusuf <221130031@students.mikroskil.ac.id>
2023-06-26 12:23:47 +0700

    update file js

--- script/main.js
+++ script/main.js
@@ -1,173 +1,221 @@
-// const allProduk = 'data/produk.json';
+const allProduk = 'data/produk.json';
 
-// const renderProduk = (allProduk) =>{
-//     let content = '';
-//     const containerCards = document.querySelector(".container-cards");
-//     allProduk.forEach(produk => {
-//         content += `
-//         <div class="card">
-//             <div class="card-img">
-//                 <img src="${produk.src}" alt="${produk.nama}">
-//             </div>
-//             <div class="container-title">
-//                 <h4 class="produk-name">${produk.nama}</h4>
-//                 <div class="star-rating">
-//                     <span class="fa fa-star checked"></span>
-//                     <span class="fa fa-star checked"></span>
-//                     <span class="fa fa-star checked"></span>
-//                     <span class="fa fa-star checked"></span>
-//                     <span class="fa fa-star checked"></span>
-//                 </div>
-//             </div>
-//             <div class="container-price">
-//                 <h5>Rp.${produk.harga}</h5>
-//                 <div class="counter">
-//                     <div class="kurang" onclick="kurangKuantiti(this.nextElementSibling)">-</div>
-//                     <div class="value">1</div>
-//                     <div class="tambah" onclick="tambahKuantiti(this.previousElementSibling)">+</div>
-//                 </div>
-//             </div>
-//             <div class="container-btn">
-//                 <button class="btn-buy" onclick="addToCart(this.parentElement.previousElementSibling.querySelector('.value').innerText)"><span class="material-symbols-outlined">shopping_cart</span>ADD TO CART</button>
-//             </div>
-//         </div>    
-//         `
-//     });
-//     containerCards.innerHTML = content;
-// }
+const renderProduk = (allProduk) =>{
+    let content = '';
+    const containerCards = document.querySelector(".container-cards");
+    allProduk.forEach(produk => {
+        content += `
+        <div class="card">
+            <div class="card-img">
+                <img src="${produk.src}" alt="${produk.nama}">
+            </div>
+            <div class="container-title">
+                <h4 class="produk-name">${produk.nama}</h4>
+                <div class="star-rating">
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                </div>
+            </div>
+            <div class="container-price">
+                <h5>Rp.${produk.harga}</h5>
+                <div class="counter">
+                    <div class="kurang" onclick="kurangKuantiti(this.nextElementSibling)">-</div>
+                    <div class="value">1</div>
+                    <div class="tambah" onclick="tambahKuantiti(this.previousElementSibling)">+</div>
+                </div>
+            </div>
+            <div class="container-btn">
+                <button class="btn-buy" onclick="add('${produk.src}','${produk.nama}',${produk.harga},parseInt(this.parentElement.previousElementSibling.querySelector('.value').innerText))"><span class="material-symbols-outlined">shopping_cart</span>ADD TO CART</button>
+            </div>
+        </div>    
+        `
+    });
+    containerCards.innerHTML = content;
+}
 
-// // Fungsi Fetch/Ambil data
-// const fetchProduk = (datas) =>{
-//     fetch(datas)
-//         .then((response) => response.json())
-//         .then(data =>{
-//             renderProduk(data.produks);
-//         })
-// }
-// fetchProduk(allProduk)
-
-
-// // Fungsi Tambah Kuantiti
-// const tambahKuantiti = (counter) => {
-//     let value = parseInt(counter.innerText);
-//     value += 1
-//     counter.innerHTML = value
-// };
-
-// // Fungsi Kurang Kuantiti
-// const kurangKuantiti = (counter) => {
-//     let value = parseInt(counter.innerText);
-//     if(value > 0){
-//         value -= 1;
-//     }
-//     counter.innerHTML = value;
-// };
+// Fungsi Fetch/Ambil data
+const fetchProduk = (datas) =>{
+    fetch(datas)
+        .then((response) => response.json())
+        .then(data =>{
+            renderProduk(data.produks);
+        })
+}
+fetchProduk(allProduk)
 
 
-// // Fungsi Searching produk
-// const searchInput = document.querySelector(".search-input");
-// const containerCards = document.querySelector(".container-cards");
+// Fungsi Tambah Kuantiti
+const tambahKuantiti = (counter) => {
+    let value = parseInt(counter.innerText);
+    value += 1
+    counter.innerHTML = value
+};
 
-// const search = () =>{
-//     searchInput.addEventListener("keyup", () => {
-//         const keyword = searchInput.value.toLowerCase();
-//         const allCards = containerCards.getElementsByClassName("card");
+// Fungsi Kurang Kuantiti
+const kurangKuantiti = (counter) => {
+    let value = parseInt(counter.innerText);
+    if(value > 1){
+        value -= 1;
+    }
+    counter.innerHTML = value;
+};
+
+
+// Fungsi Searching produk
+const searchInput = document.querySelector(".search-input");
+const containerCards = document.querySelector(".container-cards");
+
+
+const search = () =>{
+    searchInput.addEventListener("keyup", () => {
+        const keyword = searchInput.value.toLowerCase();
+        const allCards = containerCards.getElementsByClassName("card");
     
-//         Array.from(allCards).forEach((card) => {
-//             const namaProduk = card.querySelector(".produk-name").innerText.toLowerCase();
-//             if (namaProduk.includes(keyword)) {
-//                 card.style.display = "grid";
-//             } else {
-//                 card.style.display = "none";
-//             }
-//         });
-//     });
-// }
-// search();
+        Array.from(allCards).forEach((card) => {
+            const namaProduk = card.querySelector(".produk-name").innerText.toLowerCase();
+            if (namaProduk.includes(keyword)) {
+                card.style.display = "grid";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    });
+}
+search();
 
-// searchInput.addEventListener("keydown", function(event) {
-//     if (event.key === "Enter") {
-//         event.preventDefault();
-//     }
-// });
-
-
-
-// // Fungsi Filter Kategori
-
-// const kategori = document.querySelectorAll(".kategori");
-
-// const filterKategori = () => {
-//     kategori.forEach((item) => {
-//         let namaProduk = item.innerText;
-//         item.addEventListener("click", () => {
-//             item.classList.toggle("active");
-//             kategori.forEach((otherKategori) => {
-//                 if (otherKategori != item) {
-//                     otherKategori.classList.remove("active");
-//                 }
-//             });
-//             const activeKategori = document.querySelector(".kategori.active")
-//             if (!activeKategori) {
-//                 fetchProduk(allProduk)
-//             }else{
-//                 fetch(allProduk)
-//                     .then((response) => response.json())
-//                     .then(data =>{
-//                         let content = '';
-//                         const containerCards = document.querySelector(".container-cards");
-//                         data.produks.forEach(produk =>{
-//                             if (namaProduk.toLowerCase() == produk.kategori.toLowerCase()) {
-//                                 content += `
-//                                 <div class="card">
-//                                     <div class="card-img">
-//                                         <img src="${produk.src}" alt="${produk.nama}">
-//                                     </div>
-//                                     <div class="container-title">
-//                                         <h4 class="produk-name">${produk.nama}</h4>
-//                                         <div class="star-rating">
-//                                             <span class="fa fa-star checked"></span>
-//                                             <span class="fa fa-star checked"></span>
-//                                             <span class="fa fa-star checked"></span>
-//                                             <span class="fa fa-star checked"></span>
-//                                             <span class="fa fa-star checked"></span>
-//                                         </div>
-//                                     </div>
-//                                     <div class="container-price">
-//                                         <h5>Rp.${produk.harga}</h5>
-//                                         <div class="counter">
-//                                             <div class="kurang" onclick="kurangKuantiti(this.nextElementSibling)">-</div>
-//                                             <div class="value">1</div>
-//                                             <div class="tambah" onclick="tambahKuantiti(this.previousElementSibling)">+</div>
-//                                         </div>
-//                                     </div>
-//                                     <div class="container-btn">
-//                                         <button class="btn-buy"><span class="material-symbols-outlined">shopping_cart</span>ADD TO CART</button>
-//                                     </div>
-//                             </div>    
-//                                 `
-//                             };
-//                             containerCards.innerHTML = content;
-//                         })
-//                     })
-//             }
-//         })
-//     });
-// };
-
-// filterKategori();
+searchInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+    }
+});
 
 
 
-// // Add to Cart
-// let cartItemValue = document.querySelector(".cart-item");
-// const addToCart = (valueCounter) =>{
-//     // let btnCart = this;
-//     let cartValue = parseInt(cartItemValue.innerText)
-//     // console.log(typeof(cartValue))
-//     let value = parseInt(valueCounter)
-//     // console.log(typeof(value))
-//     let newValue = cartValue + value;
-//     console.log(newValue)
-//     cartItemValue.innerHTML = newValue
-// };
+// Fungsi Filter Kategori
+
+const kategori = document.querySelectorAll(".kategori");
+
+const filterKategori = () => {
+    kategori.forEach((item) => {
+        let namaProduk = item.innerText;
+        item.addEventListener("click", () => {
+            item.classList.toggle("active");
+            kategori.forEach((otherKategori) => {
+                if (otherKategori != item) {
+                    otherKategori.classList.remove("active");
+                }
+            });
+            const activeKategori = document.querySelector(".kategori.active")
+            if (!activeKategori) {
+                fetchProduk(allProduk)
+            }else{
+                fetch(allProduk)
+                    .then((response) => response.json())
+                    .then(data =>{
+                        let content = '';
+                        const containerCards = document.querySelector(".container-cards");
+                        data.produks.forEach(produk =>{
+                            if (namaProduk.toLowerCase() == produk.kategori.toLowerCase()) {
+                                content += `
+                                <div class="card">
+                                    <div class="card-img">
+                                        <img src="${produk.src}" alt="${produk.nama}">
+                                    </div>
+                                    <div class="container-title">
+                                        <h4 class="produk-name">${produk.nama}</h4>
+                                        <div class="star-rating">
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                        </div>
+                                    </div>
+                                    <div class="container-price">
+                                        <h5>Rp.${produk.harga}</h5>
+                                        <div class="counter">
+                                            <div class="kurang" onclick="kurangKuantiti(this.nextElementSibling)">-</div>
+                                            <div class="value">1</div>
+                                            <div class="tambah" onclick="tambahKuantiti(this.previousElementSibling)">+</div>
+                                        </div>
+                                    </div>
+                                    <div class="container-btn">
+                                        <button class="btn-buy"><span class="material-symbols-outlined">shopping_cart</span>ADD TO CART</button>
+                                    </div>
+                            </div>    
+                                `
+                            };
+                            containerCards.innerHTML = content;
+                        })
+                    })
+            }
+        })
+    });
+};
+
+filterKategori();
+
+
+// addToCart()
+const cart = [];
+
+const add = (image,name,price,quantity) =>{
+    const cartItem = {
+        img : image,
+        nama : name,
+        harga : price,
+        kuantitas : quantity
+    }
+    cart.push(cartItem);
+    console.log(cart)
+    let cartLenght = getTotal(cart)
+    console.log(cartLenght)
+    addToCart(cartLenght)
+};
+
+let cartItemValue = document.querySelector(".cart-item");
+const addToCart = (a) =>{
+    cartItemValue.innerHTML = a;
+};
+
+const getTotal = (cart) => {
+    let total = 0;
+    for (const item of cart) {
+        total += item.kuantitas;
+    }
+    return total
+}
+
+
+// Fitur Hover Keranjang
+const icon = document.querySelector(".icon");
+const cartWrapper = document.querySelector(".cart-wrapper");
+
+icon.addEventListener("mouseover", ()=>{
+    cartWrapper.style.display = 'block';
+})
+
+document.addEventListener("click", (e) =>{
+    const isInsideCartWrapper = cartWrapper.contains(e.target);
+    if(!isInsideCartWrapper){
+        cartWrapper.style.display = 'none';
+    }
+})
+
+// Fitur Hover Akun Saya
+const imgProfile = document.querySelector(".img-profile");
+const akunSayaWrapper = document.querySelector(".akun-saya-wrapper");
+
+imgProfile.addEventListener("mouseover", () =>{
+    akunSayaWrapper.style.display = 'flex';
+})
+
+document.addEventListener("click", (e) =>{
+    const isInsideAkunSayaWrapper = akunSayaWrapper.contains(e.target);
+    if (!isInsideAkunSayaWrapper) {
+        akunSayaWrapper.style.display = 'none';
+    }
+})
